f6c6ccfe7a5a9280403d19828e132980909c462b
witty-banner-1950 <asimsali95@gmail.com>
2019-10-01 19:26:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,16 @@
 # Your code goes here!
 class Dog
-  def name=
+  def name=(dog_name)
+    @given_name = dog_name
   end
 
   def name
+    @given_name
   end
 
   def bark
+    puts "woof!"
   end
 
 end
+
